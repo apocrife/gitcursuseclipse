a42e7484b0215c0e465365aa59287056496dd52e
Frans Giele <Frans.Giele@gmail.com>
2014-06-16 10:09:26 +0200

    Merge branch 'intfrans'
    Conflicts:
    	src/be/vdab/Main.java
    	
    	Hallo iedereen

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -4,11 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		System.out.println("Hallo iedereen");
-=======
-		System.out.println("Bonjour tout le monde");
->>>>>>> refs/heads/intfrans
 	}
 
 }
